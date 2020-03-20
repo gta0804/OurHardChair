f372001c45aff12df52607c7ee70e0682e52c09d
Shen Zhengyu <18302010009@fudan.edu.cn>
2020-03-20 20:43:33 +0800

    init

--- src/main/java/fudan/se/lab2/controller/request/RegisterRequest.java
+++ src/main/java/fudan/se/lab2/controller/request/RegisterRequest.java
@@ -2,6 +2,7 @@ package fudan.se.lab2.controller.request;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Set;
 
--- src/main/java/fudan/se/lab2/service/AuthService.java
+++ src/main/java/fudan/se/lab2/service/AuthService.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -33,7 +34,7 @@ public class AuthService {
         this.authorityRepository = authorityRepository;
     }
 
-    public User register(RegisterRequest request) {
+    public User register(@RequestBody RegisterRequest request) {
         // TODO: Implement the function.
         if (null != userRepository.findByUsername(request.getUsername())){
             return null;

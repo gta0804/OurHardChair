46bd82e197824e27a8778c277c238316e7fe4657
Shen Zhengyu <18302010009@fudan.edu.cn>
2020-04-11 14:55:52 +0800

    fix

--- src/main/java/fudan/se/lab2/service/MyRelatedConferenceService.java
+++ src/main/java/fudan/se/lab2/service/MyRelatedConferenceService.java
@@ -62,8 +62,9 @@ public class MyRelatedConferenceService {
     public boolean openSubmission(String full_name){
         Conference conference = conferenceRepository.findByFullName(full_name);
         System.out.println(conference.getFullName());
-        
+
         conference.setIsOpenSubmission(2);
+        conferenceRepository.save(conference);
         return true;
     }
 

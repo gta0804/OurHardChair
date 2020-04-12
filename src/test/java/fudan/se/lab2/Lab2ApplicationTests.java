5d69bd66bbbe4d84c74edaca153286d24619d58e
Shen Zhengyu <18302010009@fudan.edu.cn>
2020-04-12 20:36:11 +0800

    单元测试

--- src/test/java/fudan/se/lab2/Lab2ApplicationTests.java
+++ src/test/java/fudan/se/lab2/Lab2ApplicationTests.java
@@ -20,32 +20,14 @@ class Lab2ApplicationTests {
 
     @Test
     void saveAndDelete(){
-        ApplyMeeting applyMeeting = new ApplyMeeting();
-        applyMeeting.setApplicantId((long)12);
-        applyMeeting.setVerifierId(new Long(1));
-        applyMeeting.setAbbreviation("SE");
-        applyMeeting.setFullName("软件工程大会");
-        applyMeeting.setHoldingPlace("Shanghai");
-        applyMeeting.setReviewStatus(1);
-        applyMeeting.setHoldingTime((new Date()).toString());
-        ApplyMeeting applyMeeting1 = applyMeetingRepository.save(applyMeeting);
-        System.out.println(applyMeeting1.getFullName());
-        applyMeetingRepository.delete(applyMeetingRepository.findByFullName("软件工程大会"));
     }
 
     @Test
     void findById(){
-        ApplyMeeting applyMeeting = applyMeetingRepository.findById((long)(1)).get();
-        System.out.println(applyMeeting);
     }
 
     @Test
     void update(){
-        ApplyMeeting applyMeeting = new ApplyMeeting();
-        applyMeeting.setApplicantId((long)117);
-        applyMeeting.setFullName("测试测试");
-        ApplyMeeting applyMeeting1 = applyMeetingRepository.save(applyMeeting);
-        System.out.println(applyMeeting1);
     }
 
     }

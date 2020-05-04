028df9256104e0e06bf012b991afd0c9669ccf44
Shen Zhengyu <18302010009@fudan.edu.cn>
2020-05-04 15:29:12 +0800

    返回conferenceId

--- src/main/java/fudan/se/lab2/controller/MyRelatedConferenceController.java
+++ src/main/java/fudan/se/lab2/controller/MyRelatedConferenceController.java
@@ -59,7 +59,7 @@ public class MyRelatedConferenceController {
         //开始合并
         List<RelatedConferenceResponse> responseConferences = new ArrayList<>();
         for (Conference conference : conferences) {
-            RelatedConferenceResponse response = new RelatedConferenceResponse(conference.getFullName(),conference.getAbbreviation(),conference.getHoldingPlace(),conference.getHoldingTime(),conference.getSubmissionDeadline(),conference.getReviewReleaseDate(),conference.getReviewStatus(),SecurityContextHolder.getContext().getAuthentication().getName(),conference.getIsOpenSubmission(),conference.getTopics());
+            RelatedConferenceResponse response = new RelatedConferenceResponse(conference.getId(),conference.getFullName(),conference.getAbbreviation(),conference.getHoldingPlace(),conference.getHoldingTime(),conference.getSubmissionDeadline(),conference.getReviewReleaseDate(),conference.getReviewStatus(),SecurityContextHolder.getContext().getAuthentication().getName(),conference.getIsOpenSubmission(),conference.getTopics());
             responseConferences.add(response);
         }
         updateService.update(logger);
@@ -83,7 +83,7 @@ public class MyRelatedConferenceController {
         else{
             List<RelatedConferenceResponse> responseConferences = new ArrayList<>();
             for (Conference conference : conferences) {
-                RelatedConferenceResponse response = new RelatedConferenceResponse(conference.getFullName(),conference.getAbbreviation(),conference.getHoldingPlace(),conference.getHoldingTime(),conference.getSubmissionDeadline(),conference.getReviewReleaseDate(),(Integer)2,SecurityContextHolder.getContext().getAuthentication().getName(),conference.getIsOpenSubmission(),conference.getTopics());
+                RelatedConferenceResponse response = new RelatedConferenceResponse(conference.getId(),conference.getFullName(),conference.getAbbreviation(),conference.getHoldingPlace(),conference.getHoldingTime(),conference.getSubmissionDeadline(),conference.getReviewReleaseDate(),(Integer)2,SecurityContextHolder.getContext().getAuthentication().getName(),conference.getIsOpenSubmission(),conference.getTopics());
                 responseConferences.add(response);
             }
             map.put("message","获取所有我审稿的会议申请成功");
@@ -108,7 +108,7 @@ public class MyRelatedConferenceController {
         }
         List<RelatedConferenceResponse> responseConferences = new ArrayList<>();
         for (Conference conference : conferences) {
-            RelatedConferenceResponse response = new RelatedConferenceResponse(conference.getFullName(),conference.getAbbreviation(),conference.getHoldingPlace(),conference.getHoldingTime(),conference.getSubmissionDeadline(),conference.getReviewReleaseDate(),(Integer)2,SecurityContextHolder.getContext().getAuthentication().getName(),conference.getIsOpenSubmission(),conference.getTopics());
+            RelatedConferenceResponse response = new RelatedConferenceResponse(conference.getId(),conference.getFullName(),conference.getAbbreviation(),conference.getHoldingPlace(),conference.getHoldingTime(),conference.getSubmissionDeadline(),conference.getReviewReleaseDate(),(Integer)2,SecurityContextHolder.getContext().getAuthentication().getName(),conference.getIsOpenSubmission(),conference.getTopics());
             responseConferences.add(response);
         }
         map.put("message","获取所有我投稿的会议申请成功");
